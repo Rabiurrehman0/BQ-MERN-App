3c792e78c2204480654cb9606c388af08e0bc61d
Usama Usman <uusman004@gmail.com>
2023-08-22 13:37:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.SERVER_PORT
+const cors = require('cors')
+const path = require('path')
+
+app.use(cors())
+app.use('/', express.static(path.join(__dirname, './client/dist')))
+app.use(express.json())
+
+
+app.use('/api', require('./api/users/router'))
+app.use('/api', require('./api/products/router'))
+app.use('/api', require('./api/brands/router'))
+app.use('/api', require('./api/category/router'))
+app.use('/api', require('./api/mailer/router'))
+app.use('/api', require('./api/orders/router'))
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './client/dist/index.html')))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
